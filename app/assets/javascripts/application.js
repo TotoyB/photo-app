b486e69be158d110036af1a7e90dcbff8726f77b
TotoyB <tadeoalwilson.pfs@gmmail.com>
2019-11-08 15:06:00 +0800

    S10 eps 272.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= requrie jquery
+//= require jquery
+//= require twitter/bootstrap
 //= require activestorage
 //= require turbolinks
 //= require_tree .
